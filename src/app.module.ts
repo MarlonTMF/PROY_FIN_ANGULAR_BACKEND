9ec3f43890e6a34bb4b60fc6269fca708135d56f
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2024-12-19 13:54:46 -0400

    Inicio Deployado

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,25 @@ import { ProductoModule } from './producto/producto.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClienteModule } from './cliente/cliente.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+
     ProductoModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306, 
-      username:'root',
-      password:'Marlon22',
-      database:'tienda_reloj',
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT), 
+      username:process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database:process.env.POSTGRES_DATABASE,
       entities: [__dirname+'/**/*.entity{.ts,.js}'],
-      synchronize: true
+      synchronize: true, // Sincroniza la base de datos con tus entidades
     }),
     ClienteModule
   ],
